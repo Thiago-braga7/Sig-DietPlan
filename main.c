a9462d8c905f9d011c53936b3fbd98eca0af81d8
Thiago Braga <thiago.braga.135@ufrn.edu.br>
2025-08-22 12:26:21 -0300

    feat: Adiciona função Alterar Consumo Alimentos

--- main.c
+++ main.c
@@ -17,6 +17,9 @@ void menu_principal(void);
 void consumo_alimentos(void);
 void cadastrar_consumoAlimentos(void);
 void buscar_consumoAlimentos(void);
+void alterar_consumoAlimentos(void);
+
+
 
 void consumo_agua(void);
 void relatorios(void);
@@ -180,6 +183,28 @@ void buscar_consumoAlimentos(void){
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                     Registro de Consumo no dia (DD/MM/AAAA)             ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void alterar_consumoAlimentos(void){
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                               Consumo Alimentos                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                 = = = = =  Alterar Consumo de Alimentos = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                         Informe a data (DD/MM/AAAA):                    ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                     Registro de Consumo no dia (DD/MM/AAAA)             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                     Novas Informações Consumo de Alimentos              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                         Informe o Alimento:                             ///\n");
+    printf("///                         Quantidade(kcal):                               ///\n");
+    printf("///                         Informe a data (DD/MM/AAAA):                    ///\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
