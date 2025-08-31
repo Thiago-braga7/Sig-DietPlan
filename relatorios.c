c1f88faab918e8ad695a305d932c339917b8f529
GabrielUchoa17 <gabriel.uchoa.112@ufrn.>
2025-08-30 22:45:19 -0300

    Feat: Adicionando cores em relatórios

--- relatorios.c
+++ relatorios.c
@@ -2,24 +2,33 @@
 #include <stdlib.h>
 #include "relatorios.h"
 
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define CYAN    "\033[36m"
+
+
+
+
+
 char relatorios(void){
     char opcao;
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///                    = = = = =  Relatórios  = = = = =                     ///\n");
+    printf(RED"///                    = = = = =  Relatórios  = = = = =                     ///\n");
     printf("///                                                                         ///\n");
     printf("///                    1. Alimentos mais Consumidos                         ///\n");
     printf("///                    2. Total de Calorias(Dia)                            ///\n");
     printf("///                    3. Meta Diária de Calorias                           ///\n");
     printf("///                    4. Meta Diária de Água                               ///\n");
     printf("///                    5. Excluir Registro de Consumo                       ///\n");
-    printf("///                    0. Voltar ao Menu Principal                          ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                    0. Voltar ao Menu Principal                          ///\n"RESET);
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///                      Escolha a opção desejada: ");
+    printf("///                      Escolha a opção desejada: "RESET);
     scanf("%c", &opcao);
     getchar();
     return opcao;
-    
 }
