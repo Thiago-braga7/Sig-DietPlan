a278399b77696f2581afc85bf1f4610297f1eb4d
GabrielUchoa17 <gabriel.uchoa.112@ufrn.>
2025-08-30 22:42:14 -0300

    Feat: Adicionando cores em refeições

--- main.c
+++ main.c
@@ -11,12 +11,9 @@
 
 #define RED     "\033[31m"
 #define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
 #define CYAN    "\033[36m"
 #define RESET   "\033[0m"
-#define BOLD    "\033[1m"
+
 
 
 
@@ -88,8 +85,8 @@ void menu_principal(void){
     printf("///                      4. Consumo Diário(Alimentos)                       ///\n");
     printf("///                      5. Consumo Diário(Água)                            ///\n");
     printf("///                      6. Relatórios                                      ///\n");
-    printf("///                      0. Sair                                            ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n"RESET);
+    printf("///                      0. Sair                                            ///\n"RESET);
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
     printf("\n");
 }
 
--- refeicoes.c
+++ refeicoes.c
@@ -4,6 +4,16 @@
 
 
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define CYAN    "\033[36m"
+
+
+
+
+
+
 void modulo_refeicoes(void) {
     char opcao;
     do {
@@ -14,8 +24,6 @@ void modulo_refeicoes(void) {
             case '3': alterar_refeicao(); break;
             case '4': excluir_refeicao(); break;
             case '5': refeicoes_usuario(); break;
-
-            
         }
     } while (opcao != '0');  
 }
@@ -24,41 +32,42 @@ char refeicoes(void){
     char opcao;
     system("clear||cls");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///                    = = = = =   Refeições   = = = = =                    ///\n");
+    printf(RED"///                    = = = = =   Refeições   = = = = =                    ///\n");
     printf("///                                                                         ///\n");
     printf("///                    1. Cadastrar Refeição                                ///\n");
     printf("///                    2. Buscar Refeição                                   ///\n");
     printf("///                    3. Alterar Dados da Refeição                         ///\n");
     printf("///                    4. Excluir Refeição                                  ///\n");
     printf("///                    5. Listar Refeições do Usuário                       ///\n");
-    printf("///                    0. Voltar ao Menu Principal                          ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                    0. Voltar ao Menu Principal                          ///\n"RESET);
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
     printf("///                                                                         ///\n");
     printf("///                      Escolha a opção desejada: ");
     scanf("%c", &opcao);
     getchar();
     printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     return opcao;
 }
+
 void cadastrar_refeicao(void){
     char nome_refeição[100], tipo[50];
     float calorias, proteinas, carboidratos, gorduras;
     system("clear||cls");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                Refeições                                ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                                Refeições                                ///\n");
     printf("///                                                                         ///\n");
     printf("///                 = = = = =  Cadastrar Refeição = = = = =                 ///\n");
     printf("///                                                                         ///\n");
-    printf("///                         Nome da Refeição:                               ///\n");
+    printf("///                         Nome da Refeição:                               ///\n"RESET);
     scanf("%[^\n]", nome_refeição);
     getchar();
-    printf("///                         Tipo (Café, Almoço, Jantar, Lanche):            ///\n");
+    printf(RED"///                         Tipo (Café, Almoço, Jantar, Lanche):            ///\n");
     scanf("%[^\n]", tipo);
     getchar();
     printf("///                         Calorias (kcal):                                ///\n");
@@ -70,29 +79,28 @@ void cadastrar_refeicao(void){
     printf("///                         Carboidratos (g):                               ///\n");
     scanf("%f", &carboidratos);
     getchar();
-    printf("///                         Gorduras (g):                                   ///\n");
+    printf("///                         Gorduras (g):                                   ///\n"RESET);
     scanf("%f", &gorduras);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                    Refeição cadastrada com sucesso!                     ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    
+    printf("///////////////////////////////////////////////////////////////////////////////\n"RESET);
 }
+
 void buscar_refeicao(void){
     char nome_refeicao[100];
     system("clear||cls");
-
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                Refeições                                ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                                Refeições                                ///\n");
     printf("///                                                                         ///\n");
     printf("///                   = = = = =  Buscar Refeição = = = = =                  ///\n");
     printf("///                                                                         ///\n");
-    printf("///                        Informe o nome da Refeição:                      ///\n");
+    printf("///                        Informe o nome da Refeição:                      ///\n"RESET);
     scanf("%[^\n]", nome_refeicao);
     getchar(); 
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                        Informações da Refeição                          ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                        Informações da Refeição                          ///\n");
     printf("///                                                                         ///\n");
     printf("///                         Nome:                                           ///\n");
     printf("///                         Tipo:                                           ///\n");
@@ -100,27 +108,25 @@ void buscar_refeicao(void){
     printf("///                         Proteínas:                                      ///\n");
     printf("///                         Carboidratos:                                   ///\n");
     printf("///                         Gorduras:                                       ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-
-      
+    printf("///////////////////////////////////////////////////////////////////////////////\n"RESET);
 }
+
 void alterar_refeicao(void){
     char nome_refeicao[100], novo_nome_refeicao[100], novo_tipo[50];
     float novas_calorias, novas_proteinas, novos_carboidratos, novas_gorduras;
     system("clear||cls");
-
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                Refeições                                ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                                Refeições                                ///\n");
     printf("///                                                                         ///\n");
     printf("///                 = = = = =  Alterar Refeição = = = = =                   ///\n");
     printf("///                                                                         ///\n");
-    printf("///                  Informe o nome da Refeição a alterar:                  ///\n");
+    printf("///                  Informe o nome da Refeição a alterar:                  ///\n"RESET);
     scanf("%[^\n]", nome_refeicao);
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
     getchar();
     printf("///                                                                         ///\n");
-    printf("///                              Novos dados:                               ///\n");
+    printf(RED"///                              Novos dados:                               ///\n");
     printf("///                                                                         ///\n");
     printf("///                      Nome da Refeição:                                  ///\n");
     scanf("%[^\n]", novo_nome_refeicao);
@@ -137,31 +143,29 @@ void alterar_refeicao(void){
     printf("///                      Carboidratos (g):                                  ///\n");
     scanf("%f", &novos_carboidratos);
     getchar();
-    printf("///                      Gorduras (g):                                      ///\n");
+    printf("///                      Gorduras (g):                                      ///\n"RESET);
     scanf("%f", &novas_gorduras);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                    Refeição alterada com sucesso!                       ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n"RESET);
     printf("\n");
-   
-
 }
+
 void excluir_refeicao(void){
     char nome_refeicao[100], confirmacao;
     system("clear||cls");
-
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                Refeições                                ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                                Refeições                                ///\n");
     printf("///                                                                         ///\n");
     printf("///                 = = = = =  Excluir Refeição = = = = =                   ///\n");
     printf("///                                                                         ///\n");
-    printf("///                  Informe o nome da Refeição a excluir:                  ///\n");
+    printf("///                  Informe o nome da Refeição a excluir:                  ///\n"RESET);
     scanf("%[^\n]", nome_refeicao);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                         Informação da Refeição                          ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                         Informação da Refeição                          ///\n");
     printf("///                                                                         ///\n");
     printf("///                         Nome:                                           ///\n");
     printf("///                         Tipo:                                           ///\n");
@@ -170,29 +174,30 @@ void excluir_refeicao(void){
     printf("///                         Carboidratos:                                   ///\n");
     printf("///                         Gorduras:                                       ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                      Deseja excluir esta refeição?(S/N):                ///\n");
+    printf("///                      Deseja excluir esta refeição?(S/N):                ///\n"RESET);
     scanf("%c", &confirmacao);
     getchar();
     
     if (confirmacao == 'S' || confirmacao == 's') {
-        printf("///                    Refeição excluída com sucesso!                      ///\n");
+        printf(CYAN"///                    Refeição excluída com sucesso!                      ///\n"RESET);
     } else {
-        printf("///                    Operação de exclusão cancelada!                    ///\n");
+        printf(CYAN"///                    Operação de exclusão cancelada!                    ///\n"RESET);
     }
 }
+
 void refeicoes_usuario(void){
     char nome[100];
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                Refeições                                ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                                Refeições                                ///\n");
     printf("///                                                                         ///\n");
     printf("///                 = = = = =  Refeições Usuário = = = = =                  ///\n");
     printf("///                                                                         ///\n");
-    printf("///                        Informe o nome do Usuário:                       ///\n");
+    printf("///                        Informe o nome do Usuário:                       ///\n"RESET);
     scanf("%[^\n]", nome);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                           Refeições do Usuário                          ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                           Refeições do Usuário                          ///\n");
     printf("///                                                                         ///\n");
     printf("///                                 Usuário:                                ///\n");
     printf("///                                                                         ///\n");
@@ -201,7 +206,6 @@ void refeicoes_usuario(void){
     printf("///                         3. Lanche da Tarde                              ///\n");
     printf("///                         4. Jantar                                       ///\n");
     printf("///                                                                         ///\n");
-    printf("///                         Total do dia:                                   ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-  
+    printf("///                         Total do dia:                                   ///\n"RESET);
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
 }
