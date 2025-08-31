8d79e468eb7e0296e6d8821d6ebf95c830fd0823
GabrielUchoa17 <gabriel.uchoa.112@ufrn.>
2025-08-30 22:48:44 -0300

    Feat: Adicionando cores em usuarios

--- usuarios.c
+++ usuarios.c
@@ -3,6 +3,15 @@
 #include "usuarios.h"
 
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define CYAN    "\033[36m"
+
+
+
+
+
 
 void modulo_usuarios(void) {
     char opcao;
@@ -23,26 +32,22 @@ char usuarios(void){
     char opcao;
     system("clear||cls");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///                    = = = = =   Usuários   = = = = =                     ///\n");
+    printf(RED"///                    = = = = =   Usuários   = = = = =                     ///\n"RESET);
     printf("///                                                                         ///\n");
-    printf("///                    1. Cadastrar Usuário                                 ///\n");
+    printf(CYAN"///                    1. Cadastrar Usuário                                 ///\n");
     printf("///                    2. Buscar Usuário                                    ///\n");
     printf("///                    3. Alterar Dados do Usuário                          ///\n");
     printf("///                    4. Excluir Usuário                                   ///\n");
     printf("///                    0. Voltar ao Menu Principal                          ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n"RESET);
     printf("///                                                                         ///\n");
-    printf("///                      Escolha a opção desejada:                          ///\n");
+    printf("///                      Escolha a opção desejada:                          ///\n"RESET);
     scanf("%c", &opcao);
     getchar();
     return opcao;
-
-    
-    
 }
-
 
 void cadastrar_usuario(void){
     char nome[100], cpf[15], telefone[15];
@@ -50,60 +55,58 @@ void cadastrar_usuario(void){
     float peso, altura;   
     system("clear||cls");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                               Usuários                                  ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                               Usuários                                  ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf(RED"///                 = = = = =  Cadastrar Usuário = = = = =                  ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
     printf("///                                                                         ///\n");
-    printf("///                 = = = = =  Cadastrar Usuário = = = = =                  ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                         Nome Completo:                                  ///\n");
+    printf("///                         Nome Completo:                                  ///\n"RESET);
     scanf("%[^\n]", nome);
     getchar();
-    printf("///                         CPF(Apenas números):                            ///\n");
+    printf(CYAN"///                         CPF(Apenas números):                            ///\n"RESET);
     scanf("%s", cpf);
     getchar();
-    printf("///                         Telefone(Apenas números):                       ///\n");
+    printf(CYAN"///                         Telefone(Apenas números):                       ///\n"RESET);
     scanf("%s", telefone);
     getchar();
-    printf("///                         Idade:                                          ///\n");
+    printf(CYAN"///                         Idade:                                          ///\n"RESET);
     scanf("%d", &idade);
     getchar();
-    printf("///                         Peso(Kg):                                       ///\n");
+    printf(CYAN"///                         Peso(Kg):                                       ///\n"RESET);
     scanf("%f", &peso);
     getchar();
-    printf("///                         Altura(m):                                      ///\n");
+    printf(CYAN"///                         Altura(m):                                      ///\n"RESET);
     scanf("%f", &altura);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                    Usuário cadastrado com sucesso!                      ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    
-
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
+    printf(RED"///                    Usuário cadastrado com sucesso!                      ///\n"RESET);
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
 }
 
 void buscar_usuario(void){
     char cpf[15];
     system("clear||cls");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                               Usuários                                  ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                 = = = = =  Buscar Usuário = = = = =                     ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                         Informe o CPF(Apenas números):                  ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                               Usuários                                  ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf(RED"///                 = = = = =  Buscar Usuário = = = = =                     ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf("///                         Informe o CPF(Apenas números):                  ///\n"RESET);
     scanf("%s", cpf);
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                        Informações do Usuário                           ///\n");
-    printf("///                                                                         ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
+    printf(RED"///                        Informações do Usuário                           ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
     printf("///                         Nome Completo:                                  ///\n");
     printf("///                         CPF:                                            ///\n");
     printf("///                         Telefone:                                       ///\n");
     printf("///                         Idade:                                          ///\n");
     printf("///                         Peso(Kg):                                       ///\n");
     printf("///                         Altura(m):                                      ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
 }
+
 void alterar_usuario(void){
     char cpf[15], novo_cpf[15], novo_nome[100], novo_telefone[15];
     int nova_idade;
@@ -111,73 +114,71 @@ void alterar_usuario(void){
     system("clear||cls");
 
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                               Usuários                                  ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                 = = = = = Alterar Dados do Usuário = = = = =            ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                         Informe o CPF(Apenas números):                  ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                               Usuários                                  ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf(RED"///                 = = = = = Alterar Dados do Usuário = = = = =            ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf("///                         Informe o CPF(Apenas números):                  ///\n"RESET);
     scanf("%s", cpf);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                        Novos Dados do Usuário                           ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                         Nome Completo:                                  ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
+    printf(RED"///                        Novos Dados do Usuário                           ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf("///                         Nome Completo:                                  ///\n"RESET);
     scanf("%[^\n]", novo_nome);
     getchar();
-    printf("///                         CPF:                                            ///\n");
+    printf(CYAN"///                         CPF:                                            ///\n"RESET);
     scanf("%s", novo_cpf);
     getchar();
-    printf("///                         Telefone:                                       ///\n");
+    printf(CYAN"///                         Telefone:                                       ///\n"RESET);
     scanf("%s", novo_telefone);
     getchar();
-    printf("///                         Idade:                                          ///\n");
+    printf(CYAN"///                         Idade:                                          ///\n"RESET);
     scanf("%d", &nova_idade);
     getchar();
-    printf("///                         Peso(Kg):                                       ///\n");
+    printf(CYAN"///                         Peso(Kg):                                       ///\n"RESET);
     scanf("%f", &novo_peso);
     getchar();
-    printf("///                         Altura(m):                                      ///\n");
+    printf(CYAN"///                         Altura(m):                                      ///\n"RESET);
     scanf("%f", &nova_altura);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                    Usuário alterado com sucesso!                        ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
+    printf(RED"///                    Usuário alterado com sucesso!                        ///\n"RESET);
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
     printf("\n");
-    
-
 }
+
 void excluir_usuario(void){
     system("clear||cls");
     char cpf[15], confirmacao;
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                               Usuários                                  ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                 = = = = = Excluir Usuário = = = = =                     ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                         Informe o CPF(Apenas números):                  ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n");
+    printf(RED"///                               Usuários                                  ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf(RED"///                 = = = = = Excluir Usuário = = = = =                     ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf("///                         Informe o CPF(Apenas números):                  ///\n"RESET);
     scanf("%s", cpf);
     getchar();
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                           Excluindo Usuário                             ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                            Dados do Usuário                             ///\n");
-    printf("///                                                                         ///\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
+    printf(RED"///                           Excluindo Usuário                             ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
+    printf(RED"///                            Dados do Usuário                             ///\n"RESET);
+    printf(CYAN"///                                                                         ///\n");
     printf("///                         Nome Completo:                                  ///\n");
     printf("///                         CPF:                                            ///\n");
     printf("///                         Telefone:                                       ///\n");
     printf("///                         Idade:                                          ///\n");
     printf("///                         Peso(Kg):                                       ///\n");
     printf("///                         Altura(m):                                      ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf(CYAN"///////////////////////////////////////////////////////////////////////////////\n"RESET);
     printf("///                      Deseja excluir este usuário?(S/N):                  ///\n");
     scanf("%c", &confirmacao);
     getchar();
     if (confirmacao == 'S' || confirmacao == 's') {
-        printf("///                    Usuário excluído com sucesso!                      ///\n");
+        printf(RED"///                    Usuário excluído com sucesso!                      ///\n"RESET);
     } else {
-        printf("///                    Operação de exclusão cancelada!                    ///\n");
+        printf(RED"///                    Operação de exclusão cancelada!                    ///\n"RESET);
     }
-
 }
