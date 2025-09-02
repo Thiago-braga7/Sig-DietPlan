7818b3a6b5206c2ac07b1fc7fb3237786478267e
Thiago Braga <thiago.braga.135@ufrn.edu.br>
2025-09-02 09:26:20 -0300

    refactor: Remove Cores Desnecessárias dos Módulos

--- refeicoes.c
+++ refeicoes.c
@@ -6,7 +6,7 @@
 
 #define RESET  "\033[0m"
 #define RED     "\033[31m"
-#define GREEN   "\033[32m"
+
 #define CYAN    "\033[36m"
 
 
--- relatorios.c
+++ relatorios.c
@@ -5,7 +5,6 @@
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
-#define GREEN   "\033[32m"
 #define CYAN    "\033[36m"
 
 
