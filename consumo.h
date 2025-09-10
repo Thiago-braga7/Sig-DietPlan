f88bf091a1910b35cd3d6eb40e89ffac77490737
Thiago Braga <thiago.braga.135@ufrn.edu.br>
2025-09-10 17:18:42 -0300

    refactor: Unifica Módulos Consumo Alimentos e Água em um Único Módulo

---
+++ consumo.h
@@ -0,0 +1,7 @@
+void modulo_consumo(void);
+char menu_consumo(void);
+void cadastrar_consumo(void);
+void buscar_consumo(void);
+void alterar_consumo(void);
+void excluir_consumo(void);
+void quantidade_agua(void);
--- consumo_agua.h
+++
@@ -1,7 +0,0 @@
-void modulo_consumo_agua(void);
-char consumo_agua(void);
-void cadastrar_consumo_agua(void);
-void buscar_consumo_agua(void);
-void alterar_consumo_agua(void);
-void excluir_consumo_agua(void);
-void quantidade_agua(void);
