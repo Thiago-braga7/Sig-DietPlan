fbb924a3ccca8292fc4e6ea07d68de1b881054b4
Thiago Braga <thiago.braga.135@ufrn.edu.br>
2025-09-10 18:27:52 -0300

    feat: Adiciona Função Alterar Consulta

--- consultas.h
+++ consultas.h
@@ -2,5 +2,5 @@ void modulo_consultas(void);
 char consultas(void);
 void cadastrar_consulta(void);
 void buscar_consulta(void);
-// void alterar_consulta(void);
+void alterar_consulta(void);
 // void excluir_consulta(void);
