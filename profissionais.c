7ba744a6dc938069b090982c0e5c7cb544b56855
Thiago Braga <thiago.braga.135@ufrn.edu.br>
2025-10-02 08:33:35 -0300

    feat: adiciona função cadastrar profissional

--- profissionais.c
+++ profissionais.c
@@ -13,7 +13,7 @@ void modulo_profissionais(void) {
         limpar_tela();
         opcao = tela_profissionais();
         switch(opcao) {
-            // case '1': cadastrar_profissional(); break;
+            case '1': cadastrar_profissional(); break;
             // case '2': buscar_profissional(); break;
             // case '3': alterar_profissional(); break;
             // case '4': excluir_profissional(); break;
@@ -26,12 +26,12 @@ char tela_profissionais(void){
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///                  = = = = =  Agendamentos  = = = = =                     ///\n");
+    printf("///                  = = = = =  Profissionais  = = = = =                    ///\n");
     printf("///                                                                         ///\n");
-    printf("///                    1. Cadastrar Agendamento                             ///\n");
-    printf("///                    2. Buscar Agendamento                                ///\n");
-    printf("///                    3. Alterar Agendamento                               ///\n");
-    printf("///                    4. Excluir Agendamento                               ///\n");
+    printf("///                    1. Cadastrar Profissional                            ///\n");
+    printf("///                    2. Buscar Profissional                               ///\n");
+    printf("///                    3. Alterar Profissional                              ///\n");
+    printf("///                    4. Excluir Profissional                              ///\n");
     printf("///                    0. Voltar ao Menu Principal                          ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                      Escolha a opção desejada:                          ///\n");
@@ -40,3 +40,69 @@ char tela_profissionais(void){
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     return opcao;
 }
+
+void cadastrar_profissional(void){
+    FILE *arq_profissional;
+    char nome[100];
+    char cpf[13];
+    char email[30];
+    char tel[11];
+    char crn[12];
+    int id_profissional = 1;
+
+    arq_profissional = fopen("arq_profissional.csv", "rt");
+    if (arq_profissional != NULL)
+    {
+        char linha[512];
+        while (fgets(linha, sizeof(linha), arq_profissional) != NULL)
+        {
+            id_profissional++;
+        }
+        fclose(arq_profissional);
+    }
+    
+
+    limpar_tela();
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                Profissional                             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                 = = = = =  Cadastrar Profissional = = = = =             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                         Nome:                                           ///\n");
+    scanf("%s", nome); 
+    getchar();
+    printf("///                         CPF (Apenas números):                           ///\n");
+    scanf("%s", cpf); 
+    getchar();
+    printf("///                         E-mail:                                         ///\n");
+    scanf("%s", email); 
+    getchar();
+    printf("///                         Telefone (Apenas números):                      ///\n");
+    scanf("%s", tel); 
+    getchar();
+    printf("///                         CRN:                                            ///\n");
+    scanf("%s", crn); 
+    getchar();
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("                    Paciente Cadastrado com Sucesso!                         \n");
+    printf("///                        ID gerado: %02d                                    ///\n", id_profissional);
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+
+    arq_profissional = fopen("arq_profissional.csv", "at");
+
+    if (arq_profissional == NULL) {
+        printf("Erro na criacao do arquivo\n");
+        return;
+    }
+    fprintf(arq_profissional, "%d;", id_profissional);
+    fprintf(arq_profissional, "%s;", nome);
+    fprintf(arq_profissional, "%s;", cpf);
+    fprintf(arq_profissional, "%s;", email);
+    fprintf(arq_profissional, "%s;", tel);
+    fprintf(arq_profissional, "%s;", crn);
+    fclose(arq_profissional);
+    pausar();
+}
+
