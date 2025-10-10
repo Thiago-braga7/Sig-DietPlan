665583522d90a89fc7df3fee1e7515454d1ec700
Thiago Braga <thiago.braga.135@ufrn.edu.br>
2025-10-10 12:01:28 -0300

    refactor: modifica alterar agendamentos para formato binário

--- agendamentos.c
+++ agendamentos.c
@@ -158,14 +158,17 @@ void buscar_agendamento(void){
 }
 
 void alterar_agendamento(void){
-    FILE *arq_agendamentos;
-    FILE *arq_agendamentos_temp;
+    FILE * arq_agendamentos;
+    FILE * arq_agendamentos_temp;
 
-    Agendamento ag;
+    Agendamento * ag;
     int id_busca;
-    int encontrado = 0;
+    int encontrado;
     char opcao;
-    char continuar = 'S';
+    char continuar;
+
+    ag = (Agendamento*)malloc(sizeof(Agendamento));
+
     limpar_tela();
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                             Agendamentos                                ///\n");
@@ -176,27 +179,31 @@ void alterar_agendamento(void){
     scanf("%d", &id_busca); 
     getchar();
     printf("///////////////////////////////////////////////////////////////////////////////\n");
-   
-    arq_agendamentos = fopen("arq_agendamentos.csv", "rt");
-    arq_agendamentos_temp = fopen("arq_agendamentos_temp.csv", "wt");
+    encontrado = False;
+
+    
+    arq_agendamentos = fopen("arq_agendamentos.dat", "rb");
+    arq_agendamentos_temp = fopen("arq_agendamentos_temp.dat", "wb");
 
     if (arq_agendamentos == NULL || arq_agendamentos_temp == NULL){
-        printf("Erro na criacao do arquivo\n");
+        printf("Erro na criação do arquivo\n");
         return;
     }
 
-    while (fscanf(arq_agendamentos, "%d;%12[^;];%14[^;];%9[^;];%49[^;];%99[^;];%199[^\n]\n", &ag.id_agendamento, ag.cpf, ag.data, ag.hora, ag.tipo, ag.profissional, ag.observacoes) == 7) {
-        if (ag.id_agendamento == id_busca){
-            encontrado = 1;
+
+     while (fread(ag, sizeof(Agendamento), 1, arq_agendamentos)){
+        if((ag->id_agendamento == id_busca) && (ag->status == True)){
+            encontrado = True;
             do{
+                limpar_tela();
                 printf("\n    Dados atuais do agendamento    \n");
-                printf("ID:              %d\n", ag.id_agendamento);
-                printf("CPF do Paciente: %s\n", ag.cpf);
-                printf("Data:            %s\n", ag.data);
-                printf("Hora:            %s\n", ag.hora);
-                printf("Tipo:            %s\n", ag.tipo);
-                printf("Profissional:    %s\n", ag.profissional);
-                printf("Observações:     %s\n", ag.observacoes);
+                printf("ID:              %d\n", ag->id_agendamento);
+                printf("CPF do Paciente: %s\n", ag->cpf);
+                printf("Data:            %s\n", ag->data);
+                printf("Hora:            %s\n", ag->hora);
+                printf("Tipo:            %s\n", ag->tipo);
+                printf("Profissional:    %s\n", ag->profissional);
+                printf("Observações:     %s\n", ag->observacoes);
 
                 printf("\nQual campo deseja alterar?\n");
                 printf("1. CPF do Paciente\n");
@@ -212,32 +219,32 @@ void alterar_agendamento(void){
                 switch (opcao){
                     case '1':
                         printf("Novo CPF: ");
-                        scanf("%13s", ag.cpf);
+                        scanf("%13s", ag->cpf);
                         getchar();
                         break;
                     case '2':
                         printf("Nova Data (DD/MM/AAAA): ");
-                        scanf("%15s", ag.data);
+                        scanf("%15s", ag->data);
                         getchar();
                         break;
                     case '3':
                         printf("Nova Hora (HH:MM): ");
-                        scanf("%10s", ag.hora);
+                        scanf("%10s", ag->hora);
                         getchar();
                         break;
                     case '4':
                         printf("Novo Tipo: ");
-                        scanf("%50[^\n]", ag.tipo);
+                        scanf("%50[^\n]", ag->tipo);
                         getchar();
                         break;
                     case '5':
                         printf("Novo Profissional: ");
-                        scanf("%100[^\n]", ag.profissional);
+                        scanf("%100[^\n]", ag->profissional);
                         getchar();
                         break;
                     case '6':
                         printf("Novas Observações: ");
-                        scanf("%200[^\n]", ag.observacoes);
+                        scanf("%200[^\n]", ag->observacoes);
                         getchar();
                         break;
                     default:
@@ -246,36 +253,37 @@ void alterar_agendamento(void){
                 }
 
                 printf("\n    Dados atualizados    \n");
-                printf("ID:              %d\n", ag.id_agendamento);
-                printf("CPF do Paciente: %s\n", ag.cpf);
-                printf("Data:            %s\n", ag.data);
-                printf("Hora:            %s\n", ag.hora);
-                printf("Tipo:            %s\n", ag.tipo);
-                printf("Profissional:    %s\n", ag.profissional);
-                printf("Observações:     %s\n", ag.observacoes);
+                printf("ID:              %d\n", ag->id_agendamento);
+                printf("CPF do Paciente: %s\n", ag->cpf);
+                printf("Data:            %s\n", ag->data);
+                printf("Hora:            %s\n", ag->hora);
+                printf("Tipo:            %s\n", ag->tipo);
+                printf("Profissional:    %s\n", ag->profissional);
+                printf("Observações:     %s\n", ag->observacoes);
 
                 printf("\nDeseja alterar outro campo? (S/N): ");
                 scanf(" %c", &continuar);
                 continuar = confirmar_acao(continuar); 
+                if(continuar == 0){
+                    printf("Opção inválida! Digite apenas S ou N.\n");
+                }
             } while (continuar == 'S');
         }
-
-        fprintf(arq_agendamentos_temp, "%d;%s;%s;%s;%s;%s;%s\n",ag.id_agendamento, ag.cpf, ag.data, ag.hora, ag.tipo, ag.profissional, ag.observacoes);
+        fwrite(ag, sizeof(Agendamento), 1, arq_agendamentos_temp);
     }
 
     fclose(arq_agendamentos);
     fclose(arq_agendamentos_temp);
 
-    if (encontrado){
-        remove("arq_agendamentos.csv");
-        rename("arq_agendamentos_temp.csv", "arq_agendamentos.csv");
-        printf("///////////////////////////////////////////////////////////////////////////////\n");
-        printf("///                Agendamento alterado com sucesso!                        ///\n");
-        printf("///////////////////////////////////////////////////////////////////////////////\n");
+     if (encontrado == True){
+        remove("arq_agendamentos.dat");
+        rename("arq_agendamentos_temp.dat", "arq_agendamentos.dat");
+        printf("///                    Paciente alterado com sucesso!                       ///\n");
     } else {
-        remove("arq_agendamentos_temp.csv");
+        remove("arq_agendamentos_temp.dat");
         printf("\nAgendamento não encontrado!\n");
     }
+    free(ag);
     pausar();
 }
 void excluir_agendamento(void){
